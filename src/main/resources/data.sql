d3de05432d718d14f4082f063e4148c44f30f057
01Nehashri <nehashripsv.cs20@rvce.edu.in>
2024-02-02 12:41:16 +0530

    first commit

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into alien (id,name,tech) values (101,'Kiran','Blockchain');
+insert into alien (id,name,tech) values (102,'Ravi','AI');
+insert into alien (id,name,tech) values (103,'Shashi','Python');
